1348e84c9c71a56cbff07ca3dde65a5bbc5836b2
Vanja Kosanovic <end03564@adobe.com>
2022-12-07 15:48:48 +0100

    Initial commit

---
+++ src/main/java/utils/LoggerUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerUtils {
+
+    protected static final Logger log = LogManager.getLogger();
+}
---
+++ src/main/java/utils/WebDriverUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverUtils extends LoggerUtils {
+
+    public static WebDriver setUpDriver() {
+        WebDriver driver = null;
+
+
+        return driver;
+    }
+}
